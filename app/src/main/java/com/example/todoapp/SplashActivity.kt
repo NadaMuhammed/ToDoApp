bfff2b5c5391813e6e247af0d36d5485776ed702
NadaMuhammed <nadamuhammed776@gmail.com>
2024-01-25 14:16:23 +0200

    Finished Splash Screen.

---
+++ app/src/main/java/com/example/todoapp/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.todoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        startHomeActivity()
+    }
+
+    private fun startHomeActivity() {
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        },2000)
+    }
+}
