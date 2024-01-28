8969facabbc4ab4afae0725d4235dffdc6106ca4
NadaMuhammed <nadamuhammed776@gmail.com>
2024-01-28 10:16:13 +0200

    Finished Settings Fragment UI.

--- app/src/main/java/com/example/todoapp/home/screens/SettingsFragment.kt
+++ app/src/main/java/com/example/todoapp/home/screens/SettingsFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+    lateinit var binding: FragmentSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        binding.languagesAutoCompleteTv.setAdapter(ArrayAdapter(requireContext(), R.layout.drop_down_item, arrayOf("English","Arabic")))
+        binding.modeAutoCompleteTv.setAdapter(ArrayAdapter(requireContext(), R.layout.drop_down_item, arrayOf("Light","Dark")))
+        return binding.root
     }
 }
