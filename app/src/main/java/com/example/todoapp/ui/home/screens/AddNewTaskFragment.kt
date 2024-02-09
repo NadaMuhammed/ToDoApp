b06224ece80164fbdaf1f630b77867b92cdd5e23
NadaMuhammed <nadamuhammed776@gmail.com>
2024-02-09 22:26:50 +0200

    Finished Add New Task Fragment.

--- app/src/main/java/com/example/todoapp/ui/home/screens/AddNewTaskFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/home/screens/AddNewTaskFragment.kt
@@ -1,27 +1,23 @@
 package com.example.todoapp.ui.home.screens
 
 import android.annotation.SuppressLint
-import android.app.TimePickerDialog
+import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TimePicker
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentAddNewTaskBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import java.lang.String
 import java.util.Calendar
 import kotlin.Boolean
-import kotlin.Int
 
-class AddNewTaskFragment : BottomSheetDialogFragment(), TimePickerDialog.OnTimeSetListener {
+class AddNewTaskFragment : BottomSheetDialogFragment() {
     lateinit var binding: FragmentAddNewTaskBinding
-    var calendar = Calendar.getInstance()
+    var selectedDate = Calendar.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,16 +27,22 @@ class AddNewTaskFragment : BottomSheetDialogFragment(), TimePickerDialog.OnTimeS
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListeners()
+        binding.timeTv.text =
+            "${selectedDate.get(Calendar.DAY_OF_MONTH)} / ${selectedDate.get(Calendar.MONTH)+1} / ${
+                selectedDate.get(Calendar.YEAR)
+            }"
     }
 
+    @SuppressLint("SetTextI18n")
     private fun initListeners() {
         binding.addFab.setOnClickListener {
             if (validateInputs()) {
                 Toast.makeText(requireContext(), "Task Added", Toast.LENGTH_SHORT).show()
-                requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
+                dismiss()
             } else {
                 Toast.makeText(requireContext(), "Please Fill All The Fields", Toast.LENGTH_SHORT)
                     .show()
@@ -52,20 +54,26 @@ class AddNewTaskFragment : BottomSheetDialogFragment(), TimePickerDialog.OnTimeS
         binding.descriptionTil.editText!!.addTextChangedListener {
             validateInputs()
         }
-        binding.timeTv.text =
-            "${calendar.get(Calendar.HOUR_OF_DAY)}:${calendar.get(Calendar.MINUTE)}"
         binding.timeTv.setOnClickListener {
-            val timePickerDialog = TimePickerDialog(
-                context,
+            val datePicker = DatePickerDialog(
+                requireActivity(),
                 R.style.TimePickerTheme,
-                this,
-                calendar.get(Calendar.HOUR),
-                calendar.get(Calendar.MINUTE),
-                DateFormat.is24HourFormat(context)
+                { picker, year, month, day ->
+                    selectedDate.set(Calendar.YEAR, year)
+                    selectedDate.set(Calendar.MONTH, month)
+                    selectedDate.set(Calendar.DAY_OF_MONTH, day)
+                    binding.timeTv.text =
+                        "${selectedDate.get(Calendar.DAY_OF_MONTH)} / ${selectedDate.get(Calendar.MONTH)+1} / ${
+                            selectedDate.get(Calendar.YEAR)
+                        }"
+                },
+                selectedDate.get(Calendar.YEAR),
+                selectedDate.get(Calendar.MONTH),
+                selectedDate.get(Calendar.DAY_OF_MONTH)
             )
-            timePickerDialog.show()
-            timePickerDialog.getButton(TimePickerDialog.BUTTON_POSITIVE).setTextColor(Color.BLUE)
-            timePickerDialog.getButton(TimePickerDialog.BUTTON_NEGATIVE).setTextColor(Color.BLUE)
+            datePicker.show()
+            datePicker.getButton(DatePickerDialog.BUTTON_POSITIVE).setTextColor(Color.BLUE)
+            datePicker.getButton(DatePickerDialog.BUTTON_NEGATIVE).setTextColor(Color.BLUE)
         }
     }
 
@@ -86,14 +94,4 @@ class AddNewTaskFragment : BottomSheetDialogFragment(), TimePickerDialog.OnTimeS
         return isValid
     }
 
-    @SuppressLint("DefaultLocale")
-    override fun onTimeSet(timePicker: TimePicker?, hour: Int, minute: Int) {
-        val isPM: Boolean = hour >= 12
-        binding.timeTv.text = String.format("%02d:%02d %s",
-            if (hour == 12 || hour == 0) 12 else hour % 12,
-            minute,
-            if (isPM) "PM" else "AM"
-        )
-
-    }
 }
--- app/src/main/java/com/example/todoapp/ui/home/screens/ToDoListFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/home/screens/ToDoListFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentToDoListBinding
+import java.util.Calendar
 
 class ToDoListFragment : Fragment() {
+    lateinit var binding: FragmentToDoListBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do_list, container, false)
+        binding = FragmentToDoListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
